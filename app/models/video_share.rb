ffcab69275599915939da55bddb0f52519b8c618
gitReadyGitSet <76755591+gitreadygitset@users.noreply.github.com>
2021-05-02 14:56:41 -0400

    add role to sign up form

--- app/models/video_share.rb
+++
@@ -1,4 +0,0 @@
-class VideoShare < ApplicationRecord
-  belongs_to :user
-  belongs_to :video
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,13 @@
 edna = User.new(username: "edna", password: "edna11", email: "edna@edna.com", role: "supported user")
-edna.save!
+edna.save
 
 patrick = User.new(username: "patrick", password: "patrick", email: "patrick@patrick.com", role: "independent user")
-patrick.save!
+patrick.save
 
 devin = User.new(username: "devin", password: "devin1", email: "devin@devin.com", role: "supported user")
-devin.save!
+devin.save
 
-Video.find_or_create_by!(video_url: "video.com", title: "a video", user: User.find_by(username: "patrick"))
+video1 = Video.find_or_create_by!(video_url: "video.com", title: "a video", user: User.find_by(username: "patrick"))
 
-VideoShare.find_or_create_by!(user_id: 1, video_id: 1)
+edna.shared_videos << video1
+devin.shared_videos << video1
