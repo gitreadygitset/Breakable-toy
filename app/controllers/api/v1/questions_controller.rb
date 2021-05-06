10dc163fce93f10e90cfef8d733419e7d2f8c69d
gitReadyGitSet <76755591+gitreadygitset@users.noreply.github.com>
2021-05-06 14:10:58 -0400

    remove debuggers

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -4,12 +4,9 @@ class Api::V1::QuestionsController < ApplicationController
   def create
     video = Video.find(params[:video_id])
     question = Question.new(body: params["body"], video: video)
-    binding.pry
     if question.save
-      binding.pry
       render json: question
     else 
-      binding.pry
       render json: { error: question.errors.full_messages }
     end
   end
